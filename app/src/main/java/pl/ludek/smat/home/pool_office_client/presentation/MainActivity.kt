99586171ffc1682bc9890ac2f892fdf14d39cac3
Sergey90K <72564949+Sergey90K@users.noreply.github.com>
2022-07-01 00:39:27 +0300

    Prototype with relay control using buttons.

--- app/src/main/java/pl/ludek/smat/home/pool_office_client/presentation/MainActivity.kt
+++ app/src/main/java/pl/ludek/smat/home/pool_office_client/presentation/MainActivity.kt
@@ -2,6 +2,8 @@ package pl.ludek.smat.home.pool_office_client.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
@@ -10,15 +12,29 @@ import pl.ludek.smat.home.pool_office_client.R
 import pl.ludek.smat.home.pool_office_client.data.live_data.SensorsViewModel
 import pl.ludek.smat.home.pool_office_client.domain.model.PoolInfoData
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var dataFromRepository: PoolInfoViewModel
+    private lateinit var dataFromRelay: RelayViewModel
     private val dataFromSensor: SensorsViewModel by viewModels()
     private lateinit var sensorDataTextView: TextView
+    private lateinit var relayFirstOnButton:Button
+    private lateinit var relayFirstOffButton:Button
+    private lateinit var relayFiveOnButton: Button
+    private lateinit var relayFiveOffButton: Button
+    private lateinit var relayAllOnButton: Button
+    private lateinit var relayAllOffButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      dataFromRepository = ViewModelProvider(this).get(PoolInfoViewModel::class.java)
+        relayFirstOnButton = findViewById(R.id.relayOneOn)
+        relayFirstOffButton = findViewById(R.id.relayOneOff)
+        relayFiveOnButton = findViewById(R.id.relayFieveOn)
+        relayFiveOffButton = findViewById(R.id.relayFieveOff)
+        relayAllOnButton = findViewById(R.id.relayAllOn)
+        relayAllOffButton = findViewById(R.id.relayAllOff)
+        dataFromRepository = ViewModelProvider(this).get(PoolInfoViewModel::class.java)
+        dataFromRelay = ViewModelProvider(this).get(RelayViewModel::class.java)
         sensorDataTextView = findViewById(R.id.sensorDataView)
         val dataFromSensorObserver = Observer<PoolInfoData>{ inputDataFromSensor ->
             val rez:String =" T1= " + inputDataFromSensor.t1 +
@@ -27,9 +43,14 @@ class MainActivity : AppCompatActivity() {
                             " P1= " + inputDataFromSensor.p1
             sensorDataTextView.text = rez
         }
-
         dataFromSensor.currentSensorData.observe(this,dataFromSensorObserver)
         setDataToLiveData()
+        relayFirstOnButton.setOnClickListener(this)
+        relayFirstOffButton.setOnClickListener(this)
+        relayFiveOnButton.setOnClickListener(this)
+        relayFiveOffButton.setOnClickListener(this)
+        relayAllOnButton.setOnClickListener(this)
+        relayAllOffButton.setOnClickListener(this)
     }
 
      private fun setDataToLiveData(){
@@ -37,6 +58,17 @@ class MainActivity : AppCompatActivity() {
              dataFromSensor.setPoolInfoData(it)
          })
      }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.relayOneOn -> dataFromRelay.postOneRelayOn().observe(this, Observer { println(it) })
+            R.id.relayOneOff -> dataFromRelay.postOneRelayOff().observe(this, Observer { println(it) })
+            R.id.relayFieveOn -> dataFromRelay.postFiveRelayOn().observe(this, Observer { println(it) })
+            R.id.relayFieveOff -> dataFromRelay.postFiveRelayOff().observe(this, Observer { println(it) })
+            R.id.relayAllOn -> dataFromRelay.postAllRelayOn().observe(this, Observer { println(it) })
+            R.id.relayAllOff -> dataFromRelay.postAllRelayOff().observe(this, Observer { println(it) })
+        }
+    }
 }
 
 
